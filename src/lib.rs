670da53efa7e555e6b0eee2dd4eb056ea7eebf54
jacky.li <jacky.li@accenture.com>
2020-04-27 23:49:09 +0800

    add messaging handler macro

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub extern crate wascc_actor;
+
+// pub use wascc_actor::prelude::{
+//   codec, HandlerResult,
+// };
+
+#[macro_use]
+mod macros;
+
+
+#[cfg(test)]
+mod test {
+  #[macro_use]
+  use super::{actor_messaging_handlers};
+
+  
+}
