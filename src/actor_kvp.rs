090981f749097ba5bc000789718a563dbf5f2993
kevingzhang <kevin.zhang.canada@gmail.com>
2020-04-28 22:01:19 -0700

    update const

--- src/actor_kvp.rs
+++ src/actor_kvp.rs
@@ -1,14 +1,14 @@
 use tea_codec::{serialize, deserialize, keyvalue};
 use wascc_actor::prelude::*;
 
+const CAPABILITY : &'static str = "tea:keyvalue";
 
-pub fn set<'de, T: serde::Serialize + serde::Deserialize<'de>> (key: String, value: T) -> HandlerResult<T>
+pub fn set<'de, T: serde::Serialize + serde::Deserialize<'de>> (binding_name: &'static str, key: &str, value: T) -> HandlerResult<T>
 {
-  let source1 = untyped::host("bootstrap_kvp"); 
-  let req = keyvalue::SetRequest{key, value:serialize(value)?, expires_s:0};
-  let result_bytes : keyvalue::SetResponse = tea_codec::deserialize(source1.call(
-    "tea:keyvalue",
-    "Set",
+  let req = keyvalue::SetRequest{key: key.to_owned(), value:serialize(value)?, expires_s:0};
+  let result_bytes : keyvalue::SetResponse = tea_codec::deserialize(untyped::host(binding_name).call(
+    CAPABILITY,
+    tea_codec::keyvalue::OP_SET,
     serialize(req)?
   )?.as_slice())?; 
   let result: T = deserialize(result_bytes.value.as_slice())?;
