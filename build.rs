341c4808af69907350e3872c572625d00e6280cf
kevingzhang <kevin.zhang.canada@gmail.com>
2020-06-20 21:31:49 -0700

    move all  proto to one place: tea-codec

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-	prost_build::compile_protos(&["env-provider.proto"], &["../tea-runtime/proto"]).unwrap();
-	prost_build::compile_protos(&["kvp-provider.proto"], &["../tea-runtime/proto"]).unwrap();
+	prost_build::compile_protos(&["env-provider.proto"], &["../tea-codec/proto"]).unwrap();
+	prost_build::compile_protos(&["kvp-provider.proto"], &["../tea-codec/proto"]).unwrap();
 }
