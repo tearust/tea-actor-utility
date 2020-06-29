9d9ff935cfbe08242319185641dcd6006946c6d6
kevingzhang <kevin.zhang.canada@gmail.com>
2020-06-29 13:51:47 -0700

    add sign ed fn

--- src/actor_env.rs
+++ src/actor_env.rs
@@ -33,6 +33,12 @@ pub fn get_system_time(param: &str) -> HandlerResult<SystemTime>{
 }
 
 pub fn get_my_tea_id()-> HandlerResult<Vec<u8>>{
-  info!("In local test mode, use fixed c7e016fad0796bb68594e49a6ef1942cf7e73497e69edb32d19ba2fab3696596 as tea_id");
-  Ok(hex::decode("c7e016fad0796bb68594e49a6ef1942cf7e73497e69edb32d19ba2fab3696596")?)
+  info!("In local test mode, use fixed df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b as tea_id");
+  Ok(hex::decode("df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b")?)
+}
+
+pub fn get_my_tea_privkey()-> HandlerResult<Vec<u8>>{
+  info!("In local test mode, use placeholder private key fixed 5579a3c220146f0caaab49b884de505098b89326970b929d781cf4a65445a917df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b as tea_id");
+  Ok(hex::decode("5579a3c220146f0caaab49b884de505098b89326970b929d781cf4a65445a917df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b")?)
+
 }
--- src/actor_util.rs
+++ src/actor_util.rs
@@ -26,3 +26,21 @@ pub fn verify_ed25519_signature(public_key:Vec<u8>, msg:Vec<u8>, signature:Vec<u
 
   Ok(result.result)
 }
+
+pub fn sign_ed25519_signature(public_key:&Vec<u8>, private_key:&Vec<u8>, message:&Vec<u8>)->HandlerResult<Vec<u8>>{
+  let req = SignMessageRequest{
+    public_key: public_key.clone(),
+    private_key: private_key.clone(), 
+    message: message.clone(),
+  };
+
+  let buf: Vec<u8> = crate::encode_protobuf(req)?;
+  let res = untyped::default().call(
+    "tea:tpm",
+    "SignMessage",
+    buf
+  )?;
+  let result = SignMessageResponse::decode(res.as_slice())?;
+
+  Ok(result.signature)
+}
