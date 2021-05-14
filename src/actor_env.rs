cf71117991492fd731baaf78cfa28ee624b8b4d4
Yan Mingzhi <realraindust@gmail.com>
2021-05-14 16:08:58 +0800

    add time since function

--- src/actor_env.rs
+++ src/actor_env.rs
@@ -5,6 +5,7 @@ use vmh_codec::message::structs_proto::env;
 use wascc_actor::prelude::*;
 
 const CAPABILITY: &'static str = "tea:env";
+const DEFAULT_DURATION: u128 = 10;
 
 /// Return empty string is the env var is not set by the OS
 pub fn get_env_var(env_var: &str) -> anyhow::Result<String> {
@@ -22,7 +23,10 @@ pub fn get_env_var(env_var: &str) -> anyhow::Result<String> {
     if res.exists {
         Ok(res.value)
     } else {
-        Err(anyhow::anyhow!("failed to get environment variable: {}", env_var))
+        Err(anyhow::anyhow!(
+            "failed to get environment variable: {}",
+            env_var
+        ))
     }
 }
 
@@ -38,4 +42,20 @@ pub fn get_system_time() -> anyhow::Result<SystemTime> {
     let res: SystemTime =
         deserialize(response_vec.as_slice()).map_err(|e| anyhow::anyhow!("{}", e))?;
     Ok(res)
+}
+
+/// calculate elapsed time in milliseconds, if calculate duration failed returns default duration
+/// instead, default duration is defined by `DEFAULT_DURATION` const.
+pub fn time_since(earlier: SystemTime) -> anyhow::Result<u128> {
+    let now = get_system_time()?;
+    match now.duration_since(earlier) {
+        Ok(d) => Ok(d.as_millis()),
+        Err(e) => {
+            warn!(
+                "calculate duration failed: {}, returned default duration: {}",
+                e, DEFAULT_DURATION
+            );
+            Ok(DEFAULT_DURATION)
+        }
+    }
 }
