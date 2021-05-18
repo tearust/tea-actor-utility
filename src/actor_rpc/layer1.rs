6210da42d7d7a8e9e9fcf13900b332e74bd7330d
Yan Mingzhi <realraindust@gmail.com>
2021-05-18 16:12:12 +0800

    remove uuid for layer1 general request

--- src/actor_rpc/layer1.rs
+++ src/actor_rpc/layer1.rs
@@ -7,10 +7,12 @@ use vmh_codec::LAYER1_RPC_CHANNEL_NAME;
 use wascc_actor::prelude::*;
 
 pub fn commit_ra_result(req: rpc::CommitRaResultRequest, uuid: String) -> anyhow::Result<()> {
-    call_layer1_rpc(rpc::Layer1GeneralRequest {
+    call_layer1_rpc(
+        rpc::Layer1GeneralRequest {
+            msg: Some(rpc::layer1_general_request::Msg::CommitRaResultRequest(req)),
+        },
         uuid,
-        msg: Some(rpc::layer1_general_request::Msg::CommitRaResultRequest(req)),
-    })?;
+    )?;
     Ok(())
 }
 
@@ -18,24 +20,28 @@ pub fn layer1_update_node_profile(
     req: rpc::TeaNodeUpdateProfileRequest,
     uuid: String,
 ) -> anyhow::Result<()> {
-    call_layer1_rpc(rpc::Layer1GeneralRequest {
+    call_layer1_rpc(
+        rpc::Layer1GeneralRequest {
+            msg: Some(rpc::layer1_general_request::Msg::UpdateNodeProfileRequest(
+                req,
+            )),
+        },
         uuid,
-        msg: Some(rpc::layer1_general_request::Msg::UpdateNodeProfileRequest(
-            req,
-        )),
-    })?;
+    )?;
     Ok(())
 }
 
 pub fn layer1_add_new_node(tea_id: Vec<u8>, uuid: String) -> anyhow::Result<()> {
-    let rpc_res_bytes = call_layer1_rpc(rpc::Layer1GeneralRequest {
+    let rpc_res_bytes = call_layer1_rpc(
+        rpc::Layer1GeneralRequest {
+            msg: Some(rpc::layer1_general_request::Msg::AddNewNodeRequest(
+                rpc::AddNewNodeRequest {
+                    tea_id: tea_id.clone(),
+                },
+            )),
+        },
         uuid,
-        msg: Some(rpc::layer1_general_request::Msg::AddNewNodeRequest(
-            rpc::AddNewNodeRequest {
-                tea_id: tea_id.clone(),
-            },
-        )),
-    })?;
+    )?;
     let res = rpc::Layer1GeneralResponse::decode(rpc_res_bytes.as_slice())?;
     if let Some(rpc::layer1_general_response::Msg::AddNewNodeResponse(res)) = res.msg {
         if !tea_id.eq(&res.tea_id) {
@@ -50,13 +56,13 @@ pub fn layer1_add_new_node(tea_id: Vec<u8>, uuid: String) -> anyhow::Result<()> 
     Err(anyhow::anyhow!("unknown response: {:?}", res))
 }
 
-pub fn call_layer1_rpc(req: rpc::Layer1GeneralRequest) -> anyhow::Result<Vec<u8>> {
+pub fn call_layer1_rpc(req: rpc::Layer1GeneralRequest, uuid: String) -> anyhow::Result<Vec<u8>> {
     untyped::default()
         .call(
             vmh_codec::VMH_CAPABILITY_ID,
             vmh_codec::OP_OUTBOUND_MESSAGE,
             encode_protobuf(vmh::OutboundRequest {
-                uuid: req.uuid.clone(),
+                uuid,
                 channel: LAYER1_RPC_CHANNEL_NAME.into(),
                 msg: Some(vmh::outbound_request::Msg::Layer1GeneralRequest(req)),
             })?,
