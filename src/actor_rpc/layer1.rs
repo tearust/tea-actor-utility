558db8d3f1a8129036398719b22a7c690fc34e2f
Yan Mingzhi <realraindust@gmail.com>
2021-09-28 09:00:30 +0800

    add header when execute http request

--- src/actor_rpc/layer1.rs
+++ src/actor_rpc/layer1.rs
@@ -72,12 +72,13 @@ pub fn layer1_get_tapp_resource_cid(tapp_id: u64, uuid: String) -> anyhow::Resul
     Err(anyhow::anyhow!("unknown response: {:?}", res))
 }
 
-pub fn execute_http_request(req_url: &str, uuid: String) -> anyhow::Result<String> {
+pub fn execute_http_request(req_url: &str, uuid: String, headers: Vec<rpc::HttpExecutionHeader>,) -> anyhow::Result<String> {
     let rpc_res_bytes = call_layer1_rpc(
         rpc::Layer1GeneralRequest {
             msg: Some(rpc::layer1_general_request::Msg::HttpExecutionRequest(
                 rpc::HttpExecutionRequest {
                     request_url: req_url.to_string(),
+                    headers,
                 },
             )),
         },
