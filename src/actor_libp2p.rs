2e011f0cbbf0948de4a6bda5c17fc8c18f5a20c2
Yan Mingzhi <realraindust@gmail.com>
2021-11-21 16:43:50 +0800

    change conn id type to string

--- src/actor_libp2p.rs
+++ src/actor_libp2p.rs
@@ -2,7 +2,7 @@ use tea_codec::LIBP2P_CAPABILITY_ID;
 use vmh_codec::message::{encode_protobuf, structs_proto::libp2p};
 use wascc_actor::untyped;
 
-pub fn my_conn_id() -> anyhow::Result<Vec<u8>> {
+pub fn my_conn_id() -> anyhow::Result<String> {
     let conn_id = untyped::default()
         .call(
             LIBP2P_CAPABILITY_ID,
@@ -10,11 +10,11 @@ pub fn my_conn_id() -> anyhow::Result<Vec<u8>> {
             encode_protobuf(libp2p::MyConnIdRequest {})?,
         )
         .map_err(|e| anyhow::anyhow!("{}", e))?;
-    Ok(conn_id)
+    Ok(String::from_utf8(conn_id)?)
 }
 
 pub fn send_message(
-    target_conn_id: Vec<u8>,
+    target_conn_id: String,
     target_address: libp2p::RuntimeAddress,
     source_address: Option<libp2p::RuntimeAddress>,
     content: Vec<u8>,
@@ -24,7 +24,7 @@ pub fn send_message(
             LIBP2P_CAPABILITY_ID,
             "SendMessage",
             encode_protobuf(libp2p::GeneralRequest {
-                source_conn_id: vec![],
+                source_conn_id: Default::default(),
                 target_conn_id,
                 runtime_message: Some(libp2p::RuntimeMessage {
                     source_address,
@@ -47,7 +47,7 @@ pub fn pub_message(
             LIBP2P_CAPABILITY_ID,
             "PubMessage",
             encode_protobuf(libp2p::PubMessage {
-                source_conn_id: vec![],
+                source_conn_id: Default::default(),
                 runtime_message: Some(libp2p::RuntimeMessage {
                     source_address,
                     target_address: Some(target_address),
