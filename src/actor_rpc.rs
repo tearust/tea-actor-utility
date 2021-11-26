7e4a5a1ae47b01ed11f6d62ce1a28a6adb315962
Yan Mingzhi <realraindust@gmail.com>
2021-11-26 16:36:58 +0800

    add register http dispatcher request

--- src/actor_rpc.rs
+++ src/actor_rpc.rs
@@ -1,4 +1,7 @@
 pub mod adapter;
 pub mod layer1;
 
-pub use adapter::{call_adapter_rpc, ipfs_block_get, ipfs_info, register_adapter_dispatcher};
+pub use adapter::{
+    call_adapter_rpc, ipfs_block_get, ipfs_info, register_adapter_dispatcher,
+    register_adapter_http_dispatcher,
+};
